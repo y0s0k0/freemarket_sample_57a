829bff1e2873285551c170738deebf1a09f687f2
y0s0k0 <y0s0k0.0521@gmail.com>
2019-08-24 19:58:29 +0900

    ベーシック認証追加#1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,8 @@ server '3.113.175.81', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
