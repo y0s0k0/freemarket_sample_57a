d42bdb4fd1f96fa1a44935cb18ae90caab069fc4
y0s0k0 <y0s0k0.0521@gmail.com>
2019-09-02 16:33:09 +0900

    add-exhibit-view#2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+  get 'home/mypage'  => 'home#mypage'
+  get 'home/profile' => 'home#profile'
+  get 'home/logout'  => 'home#logout'
+  get 'home/identification'  => 'home#identification'
   resources :items,  only: [:new, :create, :edit, :update]
 end
