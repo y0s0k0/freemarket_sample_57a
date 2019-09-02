cdb2bdf7a4f21377b683b0469daec06ab9296e95
y0s0k0 <y0s0k0.0521@gmail.com>
2019-09-02 16:19:37 +0900

    add-exhibit-view#1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'stars' => 'stars#index'
+  resources :items,  only: [:new, :create, :edit, :update]
 end
---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20190826105430) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                  null: false
+    t.string   "image",                 null: false
+    t.string   "description",           null: false
+    t.integer  "category",              null: false
+    t.integer  "size"
+    t.integer  "brand"
+    t.integer  "condition",             null: false
+    t.integer  "postage",               null: false
+    t.integer  "region",                null: false
+    t.integer  "days",                  null: false
+    t.integer  "price",                 null: false
+    t.integer  "transaction_condition", null: false
+    t.integer  "user_id",               null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.index ["name"], name: "index_items_on_name", using: :btree
+  end
+
+end
