d8e5bcbd2fa1c1102a43722291b8df6d5fdead71
y0s0k0 <y0s0k0.0521@gmail.com>
2019-09-18 17:24:02 +0900

    Revert "Merge branch 'master' into add-exhibit-function"
    This reverts commit aa4a782ee3ac474b4ed23951be850706d099abd6, reversing
    changes made to f5813db30faabe238c9b0a333c764b0b3629bfc5.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,29 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :credit_card, as: :credit_card, dependent: :destroy
-
-  VALID_EMAIL_REGEX =                 /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  # step1入力項目
-  validates :nickname,                presence: true, length: {maximum: 20}
-  validates :email,                   presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password,                presence: true, length: {minimum: 7, maximum: 128},on: :save_to_session_before_phone
-  validates :password_confirmation,   presence: true, length: {minimum: 7, maximum: 128},on: :save_to_session_before_phone
-  validates :last_name,               presence: true
-  validates :first_name,              presence: true
-  validates :last_name_kana,          presence: true
-  validates :first_name_kana,         presence: true
-  validates :year,                    presence: true, numericality: true #数値のみ可
-  validates :month,                   presence: true, numericality: true #数値のみ可
-  validates :day,                     presence: true, numericality: true #数値のみ可
-
-  # step2入力項目
-  validates :phone_number,            presence: true, uniqueness: true, numericality: true, length: { is: 11 } #数値のみ可、11桁のみ可
-
-  # step3入力項目
-  validates :zip,                     presence: true, numericality: true, length: { is: 7 } #数値のみ可、7桁のみ可
-  validates :prefecture,              presence: true
-  validates :city,                    presence: true
-  validates :address,                 presence: true
-  validates :building,                presence: true
 end
