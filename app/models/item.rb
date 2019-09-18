67bc6dc49873f24a201da1673d66c90eb62d60be
takumi338 <qxgkh804@gmail.com>
2019-09-18 19:46:54 +0900

    create pull request

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images
-  belongs_to :category
+  # belongs_to :category
   enum condition: {brand_new: 0, near_unused: 1, no_noticeable_dirt: 2, somewhat_dirty: 3, dirty: 4, bad_condition: 5}
   enum delivery_schedule: {one_to_two_days: 0, two_to_three_days: 1, four_to_seven_days: 2}
   enum postage: {postage_included: 0, cash_on_delivery: 1}
